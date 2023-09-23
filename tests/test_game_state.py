f0cb9c33199c85d9142b42818fdff9efbc8a5168
Nathan James <nathanieljames@Nathans-MacBook-Pro-72.local>
2023-09-23 00:39:14 -0400

    yay

--- tests/test_game_state.py
+++ tests/test_game_state.py
@@ -11,4 +11,7 @@ def test_initial_values(subject):
     assert(subject.is_active(0))
 
 def test_mixed_0(subject):
-    assert(True)
+    assert(subject.make_move(6) == False)
+    assert(subject.make_move(5) == True)
+    assert(subject.scores == [1,0])
+    assert(subject.is_active(1))
