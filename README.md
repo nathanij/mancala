6b616068d769ff5416d8c7898c0a1c457bac08a8
nathanij <83778717+nathanij@users.noreply.github.com>
2023-09-24 17:53:11 -0400

    Add Basic Functionality (#1)
    * added move capabilities
    
    * initial tests
    
    * yay
    
    * Delete gameplay/__pycache__ directory
    
    * Delete tests/__pycache__ directory
    
    * added files
    
    * unit tests
    
    * valid & over
    
    * new driver-state structure
    
    * new driver-state structure, removed test
    
    * unit testing, finished state
    
    ---------
    
    Co-authored-by: Nathan James <nathanieljames@Nathans-MacBook-Pro-72.local>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 Mancala capstone project files
+
+General structure:
+    For each game, we have a game_driver object which has two players and a game_state
+    can have multiple player strategies, and the game driver will receive whose
+        turn it is from state and query that player appropriately
+    Game driver will understand when the game is over and react accordingly
+        (for now will just print to console)
+    Game state will always know whose turn it is (ex if someone has no legal
+        moves, repeat moves, etc)
--- gameplay/game_state.py
+++ gameplay/game_state.py
@@ -1,21 +1,72 @@
 class GameState:
-    # boards run left to right, towards the active player's mancala
-    # from framework of players 0 and 1 for ease of indexing
+    # game proceeds counterclockwise (right to left)
     def __init__(self, start_amt, board_size):
-        self.board_size = board_size
-        self.board = [[start_amt] * board_size for _ in range(2)]
-        self.scores = [0, 0]
-        self.active_player = False
+        self.board_size_ = board_size
+        self.board_ = [[start_amt] * board_size for _ in range(2)]
+        self.scores_ = [0, 0]
+        self.active_ = 0
 
-    # returns the board state from the POV of the active player
-    def get_state(self, player):
-        opp = (player + 1) % 2
-        return self.scores[player], self.scores[opp], self.board[player] + self.board[opp]
-    
-    def swap_active(self):
-        self.active_player = not self.active_player
-    
-    # returns true if move is valid, false otherwise
-    def make_move(self, player, idx):
-        if idx < 0 or idx >= self.board_size:
+    # returns whether the game is over
+    def finished(self):
+        if sum(self.board_[0]) != 0 and sum(self.board_[1]) != 0:
             return False
+        self.scores_[0] += sum(self.board_[0])
+        self.scores_[1] += sum(self.board_[1])
+        return True
+
+    # returns which player is active to the driver
+    def active_player(self):
+        return self.active_
+
+    # returns the board state from the POV of the given player
+    def get_player_state(self, player):
+        if player == 0:
+            return (self.scores_, self.board_)
+        return (list(reversed(self.scores_)), list(reversed(self.board_)))
+    
+    def is_valid_move_(self, idx):
+        if idx < 0 or idx >= self.board_size_:
+            return False
+        return self.board_[self.active_][idx] > 0
+    
+    def capture_(self, idx):
+        oppo = self.board_size_ - idx - 1
+        self.board_[self.active_][idx] += self.board_[not self.active_][oppo]
+        self.board_[not self.active_][oppo] = 0
+    
+    # makes move inputted by runner
+    def make_move(self, idx):
+        if not self.is_valid_move_(idx):
+            return False
+        stones = self.board_[self.active_][idx]
+        self.board_[self.active_][idx] = 0
+        side = self.active_
+
+        while stones > 0:
+            idx -= 1
+            if idx == -1:
+                if side == self.active_:
+                    self.scores_[side] += 1
+                    stones -= 1
+                side = not side
+                idx = self.board_size_
+            else:
+                self.board_[side][idx] += 1
+                stones -= 1
+
+        if idx != self.board_size_:
+            if side == self.active_ and self.board_[side][idx] == 1:
+                self.capture_(idx)
+            self.active_ = not self.active_
+        return True
+    
+    def outcome(self):
+        dif = self.scores_[0] - self.scores_[1]
+        if dif == 0:
+            return -1
+        if dif > 0:
+            return 0
+        return 1
+    
+    def get_scores(self):
+        return self.scores_
